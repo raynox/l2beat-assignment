06e5f7316776f851118c4b172056c2e3338a0a8f
Jakub Saleniuk <jakub.saleniuk@gmail.com>
2025-11-16 21:30:14 +0100

    Change cron to each 10 minutes

--- src/players/services/tasks.service.ts
+++ src/players/services/tasks.service.ts
@@ -15,7 +15,7 @@ export class TasksService {
     this.maxPlayers = this.configService.get('MAX_PLAYERS') ?? 10;
   }
 
-  @Cron(CronExpression.EVERY_MINUTE)
+  @Cron(CronExpression.EVERY_10_MINUTES)
   async handleCron() {
     await this.playersService.refreshScores(this.maxPlayers);
   }
